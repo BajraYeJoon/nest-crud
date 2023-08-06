3f8d5c125e121db134c389977a23c4c77aab0e10
bajrayejoon <binaya.bajrashakya@gmail.com>
2023-08-06 12:28:04 +0545

    controllers marked as done

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -2,40 +2,104 @@ import {
   Controller,
   Get,
   Post,
-  Req,
-  HttpCode,
-  Header,
+  Body,
   Param,
+  Delete,
+  Put,
+  Query,
 } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  CreateRecipeDto,
+  UpdateRecipeDto,
+  ListAllEntities,
+} from './dto/create-recipe.dto';
 
 @Controller('recipes')
 export class RecipesController {
   @Post()
-  //custom header
-  @Header('Cache-Control', 'none')
-  //to change the status code
-  @HttpCode(204)
-  create(): string {
-    return 'This action adds a new recipe';
+  create(@Body() createRecipeDto: CreateRecipeDto) {
+    return createRecipeDto;
   }
 
-  //to fetch the receipes from the GET
-  @Get() // Decorator
-  findAll(@Req() request: Request): string {
-    return 'This action returns all recipes';
+  @Get()
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all recipes (limit: ${query.limit} items)`;
   }
 
-  //To fetch the single receipes from the GET
   @Get(':id')
-  findOne(@Param() params: any): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} recipe`;
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} recipe`;
   }
 
-  //wildcard route
-  //  @Get('adb*adf')
-  //   findAll2(): string {
-  //     return 'This route uses a wildcard';
-  //   }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
+    return `This action updates a #${id} recipe`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} recipe`;
+  }
+
+  @Get('aa')
+  sdfg(@Query() query: ListAllEntities) {
+    return `This action returns all recipes (limit: ${query.limit} items)`;
+  }
 }
+
+// import {
+//   Controller,
+//   Get,
+//   Post,
+//   Req,
+//   HttpCode,
+//   Header,
+//   Param,
+//   Body,
+// } from '@nestjs/common';
+// import { CreateRecipeDto } from './dto/create-recipe.dto';
+// import { Request } from 'express';
+// import { create } from 'domain';
+
+// @Controller('recipes')
+// export class RecipesController {
+// //   @Post()
+// //   //custom header
+// //   @Header('Cache-Control', 'none')
+// //   //to change the status code
+// //   @HttpCode(204)
+// //   create(): string {
+// //     return 'This action adds a new recipe';
+//   }
+
+//   //to fetch the receipes from the dto
+//   @Post()
+//   async create(@Body() createRecipeDto: CreateRecipeDto){
+//     return `This action adds a new recipe ${createRecipeDto.name}`;
+//   }
+
+//   //to fetch the receipes from the GET
+//   @Get() // Decorator
+//   findAll(@Req() request: Request): string {
+//     return 'This action returns all recipes';
+//   }
+
+//   //For async
+//   //   @Get()
+//   //   async findAllAsync(): Promise<any[]> {
+//   //     return [];
+//   //   }
+
+//   //To fetch the single receipes from the GET
+//   @Get(':id')
+//   findOne(@Param() params: any): string {
+//     console.log(params.id);
+//     return `This action returns a #${params.id} recipe`;
+//   }
+
+//   //wildcard route
+//   //  @Get('adb*adf')
+//   //   findAll2(): string {
+//   //     return 'This route uses a wildcard';
+//   //   }
+// }
