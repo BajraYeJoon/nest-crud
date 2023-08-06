358d7a1117a188c9290d86bf9e9cb745b0ebfdb1
bajrayejoon <binaya.bajrashakya@gmail.com>
2023-08-06 13:28:10 +0545

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
-
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { RecipesModule } from './recipes/recipes.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import path from 'path';
 
 @Module({
   imports: [RecipesModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'recipes', method: RequestMethod.GET });
+  }
+}
